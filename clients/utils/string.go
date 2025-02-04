c879e3bbfb1c150526c3e3e399001ba455a0c761
Ashutosh Das <git.pyprism@gmail.com>
2025-02-05 00:49:04 +0600

    added utils package

---
+++ clients/utils/string.go
@@ -0,0 +1,13 @@
+package utils
+
+import "math/rand"
+
+// RandStringBytes generates a random string of n bytes
+func RandStringBytes(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
