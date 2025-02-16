2a817877edecd8df9083206bfeafd3e608d6f8b4
Ashutosh Das <git.pyprism@gmail.com>
2025-02-16 21:49:33 +0600

    removed unused codes

--- server/main.go
+++ server/main.go
@@ -4,18 +4,6 @@ import (
 	"device-chronicle-server/cmd"
 )
 
-//
-//func main() {
-//	r := gin.Default()
-//	wsServer := NewWebSocketServer()
-//
-//	// WebSocket endpoint
-//	r.GET("/ws", wsServer.handleClient)
-//	r.GET("/clients", wsServer.listClients)
-//	// Run server
-//	r.Run(":8080")
-//}
-
 func main() {
 	cmd.Execute()
 }
