dfb4a26d080a3f3a4bd207933f285ba3d71e8e3c
Ashutosh Das <git.pyprism@gmail.com>
2025-02-18 22:33:20 +0600

    removed static file controller

--- server/config/routes.go
+++ server/config/routes.go
@@ -7,10 +7,8 @@ import (
 
 func RegisterRoutes(router *gin.Engine) {
 	wsServer := controllers.NewWebSocketServer()
-	staticFile := controllers.StaticFileController{}
 	router.GET("/ws", wsServer.HandleClient)
 	router.GET("/analytics/:client_id", wsServer.ServeAnalyticsPage)
 	router.GET("/analytics_ws/:client_id", wsServer.HandleAnalytics)
 	router.GET("/clients", wsServer.ListClients)
-	router.GET("/:static", staticFile.StaticFile)
 }
