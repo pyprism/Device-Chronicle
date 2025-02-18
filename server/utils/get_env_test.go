5b89932e4f160133e1f0e7d7ef551978ef511ba3
Ashutosh Das <git.pyprism@gmail.com>
2025-02-18 23:39:25 +0600

    mooaareeee test

---
+++ server/utils/get_env_test.go
@@ -0,0 +1,21 @@
+package utils_test
+
+import (
+	"device-chronicle-server/utils"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestGetEnv(t *testing.T) {
+	// Test case: No environment variable set
+	os.Unsetenv("MY_ENV")
+	value := utils.GetEnv("MY_ENV", "default_value")
+	assert.Equal(t, "default_value", value)
+
+	// Test case: Environment variable set
+	os.Setenv("MY_ENV", "actual_value")
+	defer os.Unsetenv("MY_ENV")
+	value = utils.GetEnv("MY_ENV", "default_value")
+	assert.Equal(t, "actual_value", value)
+}
